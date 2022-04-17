f9f30157fc57d5a403027299ce96e312857e5fb8
BarnesToby <lehre.toby@gmail.com>
2022-04-17 13:45:13 +0000

    test publish

--- main.cpp
+++ main.cpp
@@ -1,80 +1,52 @@
-/*
- * Main.cpp
- * Copyright (c) 2022, ZHAW
- * All rights reserved.
- */
-
-#include <stdio.h>
-#include <mbed.h>
-// #include <EthernetInterface.h>
-#include "EthernetInterface.h"
-#include "NetworkInterface.h"
-
+#include "mbed.h"
+#include "rtos.h"
 #include <stdio.h>
 #include <string.h>
-
-//#include <MQTTNetwork.h>
-//#include <MQTTClient.h>
-//#include <MQTTmbed.h> // Countdown
+#include <mbed.h>
+#include "EthernetInterface.h"
+#include "NetworkInterface.h"
+#include <stdio.h>
+#include <string.h>
 #include <MQTTClientMbedOs.h>
+
+
+#define COM_FLAG 1
+
+Thread communication_thread;
+
+void positionRequest(MQTT::MessageData& md);
+void sonnenSegel(MQTT::MessageData& md);
+void altitudeRequest(MQTT::MessageData& md);
+void azimuthRequest(MQTT::MessageData& md);
+
+void communication();
+
 extern "C" void mbed_mac_address(char *s);
-
 char printbuf[100];
-
 int arrivedcount = 0;
+const char* hostname = "192.168.50.223";
+int port = 1883;
 
-void messageArrived(MQTT::MessageData& md)
-{
-    MQTT::Message &message = md.message;
-    sprintf(printbuf, "Message %d arrived: qos %d, retained %d, dup %d, packetid %d\n", 
-		++arrivedcount, message.qos, message.retained, message.dup, message.id);
-    printf("Payload %.*s\n", (int)message.payloadlen, (char*)message.payload);
+
+// Spawns a thread to run blink for 5 seconds
+int main() {
+	UnbufferedSerial* serial = new UnbufferedSerial(PG_14, PG_9);
+	
+    communication_thread.start(callback(communication));
+    ThisThread::sleep_for(5000);
+    communication_thread.flags_set(COM_FLAG);
+    communication_thread.join();
 }
 
-int main() {
-    
-    // create miscellaneous periphery objects
-    
-    DigitalOut led(LED1);
-    
-    ThisThread::sleep_for(500ms);
-    
-    UnbufferedSerial* serial = new UnbufferedSerial(PG_14, PG_9);
-    
-    // create ethernet interface
-    
-    printf("Create ethernet interface and webserver (please wait!)...\r\n");
-    
-    // DigitalOut enableRouter(PB_15);
-    // enableRouter = 1;
+// Blink function toggles the LED in a long running loop
+void communication() {
 
-    // Achtung umbedingt mbed_lib.json file importieren
-    /*
-    NetworkInterface *net = NetworkInterface::get_default_instance();
-    MQTTNetwork mqttNet(net);
-    MQTT::Client<MQTTNetwork, Countdown> client(mqttNet);
-
-    const char* hostname = "192.168.0.10";
-    int port = 1883;
-
-    mqttNet.connect(hostname, port);
-    //mqttNet.connect(mqtt_global::hostname, mqtt_global::port);*/
-
-    const char* hostname = "192.168.50.223";
-    int port = 1883;
-    
-    /*NetworkInterface *net = NetworkInterface::get_default_instance();
-    TCPSocket socket;
-    MQTTClient client(&socket);
-    socket.open(net);
-    socket.connect(hostname, port);
-    //socket.connect(hostname, port);*/
-
-    NetworkInterface *net = EthInterface::get_default_instance();
+    MQTT::Message message;	
+	
+	NetworkInterface *net = EthInterface::get_default_instance();
 
     if (!net) {
         printf("Error! No network inteface found.\n");
-        return 0;
     }
     net->set_dhcp(true);
     //net->set_network("192.168.0.20", "255.255.255.0", "192.168.0.1");
@@ -100,23 +72,76 @@ int main() {
 
     data.clientID.cstring = (char *)"MQTT_CONNECT";
     client.connect(data);
-    int rc;
-    rc = client.subscribe("a", MQTT::QOS1, messageArrived);
+    if(client.subscribe("Anfrage/Position", MQTT::QOS1, positionRequest) != NSAPI_ERROR_OK){
+        // error Handler
+    }
+    if(client.subscribe("Anfrage/Sonnensegel", MQTT::QOS1, sonnenSegel) != NSAPI_ERROR_OK){
+        // error Handler
+    }
+    if(client.subscribe("Anfrage/Sonnenparameter/Altitude", MQTT::QOS1, altitudeRequest) != NSAPI_ERROR_OK){
+        // error Handler
+    }
+    if(client.subscribe("Anfrage/Sonnenparameter/Azimuth", MQTT::QOS1, azimuthRequest) != NSAPI_ERROR_OK){
+        // error Handler
+    }
 
-    while (true) {
+    
+    const char* test_topic = "Fahrzeugparameter/Location";
+	
+    while (!ThisThread::flags_wait_any_for(COM_FLAG, 1000)) {
+        static MQTT::Message pubmsg;
 
-        MQTT::Message message;
+        static char testchar[] = "5.27/3.69";
+        // strcpy(testchar, (char) 10);
 
-        arrivedcount = 0;
+        memset(&pubmsg, '\0', sizeof(pubmsg));
+        pubmsg.payload = (void*)"a much longer message that we can shorten to the extent that we need to payload up to 11";
+        pubmsg.payloadlen = 11;
+        pubmsg.qos = MQTT::QOS1;
+        pubmsg.retained = false;
+        pubmsg.dup = false;
 
-        /*char buf[100] = "hello there";
-        message.qos = MQTT::QOS1;
-        message.payloadlen = strlen(buf)+1;
-        message.payload = (void*)buf;
-        rc = client.publish("test", message);*/
-        while (arrivedcount == 0)
-            client.yield(1000);      
+        if(client.publish(test_topic, pubmsg) != NSAPI_ERROR_OK){
+            //error Handler
+        }
 
-        //ThisThread::sleep_for(250ms);
+        /* wait for the message to be received */
+        uint8_t wait_seconds = 10;
+        while (wait_seconds-- > 0){
+            client.yield(100);
+        }
     }
 }
+
+void positionRequest(MQTT::MessageData& md)
+{
+    MQTT::Message &message = md.message;
+    sprintf(printbuf, "Message %d arrived: qos %d, retained %d, dup %d, packetid %d\n", 
+		++arrivedcount, message.qos, message.retained, message.dup, message.id);
+    printf("Payload %.*s\n", (int)message.payloadlen, (char*)message.payload);
+}
+
+void sonnenSegel(MQTT::MessageData& md)
+{
+    MQTT::Message &message = md.message;
+    sprintf(printbuf, "Message %d arrived: qos %d, retained %d, dup %d, packetid %d\n", 
+		++arrivedcount, message.qos, message.retained, message.dup, message.id);
+    printf("Payload %.*s\n", (int)message.payloadlen, (char*)message.payload);
+}
+
+void altitudeRequest(MQTT::MessageData& md)
+{
+    MQTT::Message &message = md.message;
+    sprintf(printbuf, "Message %d arrived: qos %d, retained %d, dup %d, packetid %d\n", 
+		++arrivedcount, message.qos, message.retained, message.dup, message.id);
+    printf("Payload %.*s\n", (int)message.payloadlen, (char*)message.payload);
+}
+
+void azimuthRequest(MQTT::MessageData& md)
+{
+    MQTT::Message &message = md.message;
+    sprintf(printbuf, "Message %d arrived: qos %d, retained %d, dup %d, packetid %d\n", 
+		++arrivedcount, message.qos, message.retained, message.dup, message.id);
+    printf("Payload %.*s\n", (int)message.payloadlen, (char*)message.payload);
+}
+
